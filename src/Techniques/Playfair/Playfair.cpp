a47da253bcad420916fdaef765a4c9bd6af91863
RobbeNooyens <robbenooyens@hotmail.com>
2022-12-11 01:34:48 +0100

    Rewrite p1

--- src/Techniques/Playfair/Playfair.cpp
+++ src/Techniques/Playfair/Playfair.cpp
@@ -6,15 +6,10 @@
 #include <algorithm>
 #include <cmath>
 #include <utility>
-#include <vector>
 #include <random>
 #include <sstream>
 #include <iostream>
 
-#define HILLCLIMB_AMOUNT 100
-#define HILLCLIMB_DEPTH 10000
-
-// nqbuthlmafekzivprwocyxdsg : 0299568
 
 using namespace std;
 
@@ -24,27 +19,9 @@ double Playfair::getFrequencyScore(const string &decoded) {
     for(auto const& frequency: letterFrequency) {
         double observed = (double) count(decoded.begin(), decoded.end(), frequency.first);
         observed /= length;
-//        score += pow(observed - predicted, 2);
-        score += abs(observed - frequency.second);
+        score += std::sqrt(std::pow(observed - frequency.second, 2));
     }
     return score;
-}
-
-Playfair::Playfair(string  cipher): cipherText(move(cipher)) {}
-
-void Playfair::decrypt() {
-    // Construct a square 1000 times
-    std::random_device random_dev;
-    std::mt19937       generator(random_dev());
-    string characters = "abcdefghiklmnopqrstuvwxyz";
-    double score = std::numeric_limits<double>::infinity();
-    string config;
-    for(int i = 0; i < HILLCLIMB_AMOUNT; i++) {
-        shuffle(characters.begin(), characters.end(), generator);
-        hillClimbing(characters, score, config);
-        cout << i << ":\t" << score << " : " << config << endl;
-    }
-    cout << decode(characters) << endl;
 }
 
 void Playfair::transform(const string &config, char &c1, char &c2, int sign) {
@@ -74,42 +51,12 @@ void Playfair::transform(const string &config, char &c1, char &c2, int sign) {
     c2 = config.at(index2);
 }
 
-double Playfair::decodeScore(string &config) {
-    stringstream decoded;
-    bool digram = false, potential_double = false;
-    char c1, c2, doubled;
-    for (char const &c: this->cipherText) {
-        if (!digram) {
-            c1 = c;
-            digram = true;
-        } else {
-            c2 = c;
-            digram = false;
-            transform(config, c1, c2, -1);
-            if (potential_double) {
-                if(c1 == doubled) {
-                    decoded << c1;
-                } else {
-                    decoded << c1 << 'x';
-                }
-            }
-            if (c2 == 'x') {
-                doubled = c1;
-                potential_double = true;
-            } else {
-                decoded << c1 << c2;
-            }
-        }
-    }
-    return this->getFrequencyScore(decoded.str());
-}
-
-string Playfair::decode(const string &config) {
+string Playfair::decode(const string &config, const string & cipher) {
     stringstream decoded;
     bool digram = false;
     bool potential_double = false;
     char c1, c2, doubled;
-    for (char const c: this->cipherText) {
+    for (char const c: cipher) {
         if (!digram) {
             c1 = c;
             digram = true;
@@ -135,37 +82,33 @@ string Playfair::decode(const string &config) {
     return decoded.str();
 }
 
-void Playfair::hillClimbing(string &charSeq, double &bestScore, string& bestConfig) {
-    double score = this->decodeScore(charSeq);
-    string config(charSeq.begin(), charSeq.end());
-    srand((unsigned) time(nullptr));
-    int improve_timer = 0;
-    for(int i = 0; i < HILLCLIMB_DEPTH; i++, improve_timer++) {
-//        if(improve_timer > 100) {
-////            cout << i << endl;
-//            break;
-//        }
-        int index1 = rand() % (charSeq.size());
-        int index2 = rand() % (charSeq.size());
-        char temp = charSeq.at(index1);
-        charSeq.at(index1) = charSeq.at(index2);
-        charSeq.at(index2) = temp;
-        double new_score = this->decodeScore(charSeq);
-        if(new_score < score) {
-            improve_timer = 0;
-            score = new_score;
-            config = string(charSeq.begin(), charSeq.end());
+string Playfair::encode(const string &config, const string& input) {
+    stringstream encoded;
+    bool digram = false;
+    char c1, c2;
+    for (char const &c: input) {
+        if (!digram) {
+            c1 = c;
+            digram = true;
+        } else {
+            if (c1 == c) {
+                c2 = 'x';
+                transform(config, c1, c2, 1);
+                encoded << c1 << c2;
+                c1 = c;
+            } else {
+                c2 = c;
+                digram = false;
+                transform(config, c1, c2, 1);
+                encoded << c1 << c2;
+            }
         }
     }
-    if(score < bestScore) {
-        bestScore = score;
-        bestConfig = config;
-    }
-
+    return encoded.str();
 }
 
 double Playfair::energy(const string& config) {
-    return getFrequencyScore(decode(config));
+    return getFrequencyScore(decode(config, this->cipherText));
 }
 
 double Playfair::temperature(int k, int kMax) {
@@ -194,9 +137,9 @@ void progressBar(double progress, long long remaining=0, int barWidth=70) {
     std::cout.flush();
 }
 
-string Playfair::simulatedAnnealing(int kMax) {
+string Playfair::simulatedAnnealing(const std::string &cipher, int kMax=0) {
     // Save best shot
-    string bestConfig = "bwxhoitqmeaflsurdzpgnkyvc";
+    string bestConfig;
     double bestScore = 1000000000;
     string s = "abcdefghiklmnopqrstuvwxyz";
     shuffle(s.begin(), s.end(), default_random_engine(chrono::system_clock::now().time_since_epoch().count()));
@@ -224,7 +167,6 @@ string Playfair::simulatedAnnealing(int kMax) {
         if(energyNext < bestScore) {
             bestConfig = string(s_next);
             bestScore = energyNext;
-//            cout << "New best: " << bestScore << "\tConfig: " << bestConfig << "\t" << decode(bestConfig) << endl;
         }
         // Calculate probability
         double P = 0;
@@ -244,20 +186,17 @@ string Playfair::simulatedAnnealing(int kMax) {
             auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
             auto remaining = (elapsed/(k+1))*(kMax-(k+1));
             progressBar((double) k/kMax, remaining);
-//            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-//            cout << "K: " << k << "\tT: " << T << "\tE: " << energy(s) << "\tS: " << s;
-//            cout << "\tTime: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms" << endl;
         }
     }
     cout << "Best: " << bestScore << "\tConfig: " << bestConfig << endl;
-    cout << "Decrypted: " << decode(bestConfig) << endl;
+    cout << "Decrypted: " << decode(bestConfig, cipher) << endl;
 
     cout << "Current: " << s << endl;
-    cout << "Decrypted: " << decode(s) << endl;
+    cout << "Decrypted: " << decode(s, cipher) << endl;
     return s;
 }
 
-string Playfair::simulatedAnnealing(int kMax, int max_misses) {
+string Playfair::simulatedAnnealing(const string &cipher, int kMax, int max_misses) {
     // Save best shot
     string bestConfig = "bwxhoitqmeaflsurdzpgnkyvc";
     double bestScore = 1000000000;
@@ -319,10 +258,10 @@ string Playfair::simulatedAnnealing(int kMax, int max_misses) {
         }
     }
     cout << "Best: " << bestScore << "\tConfig: " << bestConfig << endl;
-    cout << "Decrypted: " << decode(bestConfig) << endl;
+    cout << "Decrypted: " << decode(bestConfig, cipher) << endl;
 
     cout << "Current: " << s << endl;
-    cout << "Decrypted: " << decode(s) << endl;
+    cout << "Decrypted: " << decode(s, cipher) << endl;
     return s;
 }
 
--- src/Techniques/Playfair/Playfair.h
+++ src/Techniques/Playfair/Playfair.h
@@ -13,45 +13,13 @@ class Playfair {
 public:
     explicit Playfair(std::string  cipher);
 
-    void decrypt();
     std::string simulatedAnnealing(int kMax);
-
-    std::string decode(const std::string &config);
-
-//    const std::map<char, double> letterFrequency = {
-//            {'a', 0.0849},
-//            {'b', 0.0207},
-//            {'c', 0.0453},
-//            {'d', 0.0338},
-//            {'e', 0.1116},
-//            {'f', 0.0181},
-//            {'g', 0.0247},
-//            {'h', 0.0300},
-//            {'i', 0.0774},
-//            {'k', 0.0110},
-//            {'l', 0.0548},
-//            {'m', 0.0301},
-//            {'n', 0.0665},
-//            {'o', 0.0716},
-//            {'p', 0.0316},
-//            {'q', 0.0019},
-//            {'r', 0.0758},
-//            {'s', 0.0573},
-//            {'t', 0.0695},
-//            {'u', 0.0363},
-//            {'v', 0.0100},
-//            {'w', 0.0128},
-//            {'x', 0.0029},
-//            {'y', 0.0177},
-//            {'z', 0.0027}
     std::string simulatedAnnealing(int kMax, int max_misses);
+
+    std::string decode(const std::string &config, const std::string &input);
+    std::string encode(const std::string &config, const std::string &input);
+
 private:
-
-    double getFrequencyScore(const std::string& decoded);
-
-    const std::string cipherText;
-
-//    };
 
     const std::map<char, double> letterFrequency = {
             {'a', 0.0812},
@@ -81,13 +49,11 @@ private:
             {'z', 0.0007}
     };
 
+    double getFrequencyScore(const std::string& decoded);
+
     static void transform(const std::string &config, char &c1, char &c2, int sign);
 
-    double decodeScore(std::string &config);
-
-    void hillClimbing(std::string &charSeq, double &bestScore, std::string &bestConfig);
-
-    double temperature(int k, int kMax);
+    static double temperature(int k, int kMax);
 
     double energy(const std::string &config);
 };
